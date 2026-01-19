74370071522218df7e4043ae190fa5d3acbbdd42
cuadra <jose.cuadra@live.com>
2026-01-19 15:02:34 -0500

    welcome view fixes

--- src/components/layouts/Message/Message.styles.ts
+++ src/components/layouts/Message/Message.styles.ts
@@ -2,15 +2,19 @@ import { styled } from "@macaron-css/solid";
 import { themeContract } from "../../../styles/contracts/theme";
 const MessageStyled = styled("div", {
   base: {
+    padding: "40px",
     width: "100%",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     flexDirection: "column",
+    boxSizing: "border-box",
   },
 });
-const H2Styled = styled("h2", {
+const H1Styled = styled("h1", {
   base: {
+    margin: "0",
+    padding: "0",
     textAlign: "center",
     color: themeContract.colors.color.primary,
     fontFamily: themeContract.fonts.families.primary,
@@ -20,4 +24,30 @@ const H2Styled = styled("h2", {
     flexDirection: "column",
   },
 });
-export { MessageStyled, H2Styled };
+const H2Styled = styled("h2", {
+  base: {
+    margin: "0",
+    padding: "0",
+    textAlign: "center",
+    color: themeContract.colors.color.primary,
+    fontFamily: themeContract.fonts.families.primary,
+    fontSize: themeContract.fonts.sizes.xlarge,
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+    opacity: "0.3",
+  },
+});
+const PStyled = styled("p", {
+  base: {
+    color: themeContract.colors.color.primary,
+    fontSize: themeContract.fonts.sizes.large,
+  },
+});
+const AStyled = styled("a", {
+  base: {
+    color: themeContract.colors.color.primary,
+  },
+});
+
+export { MessageStyled, H1Styled, H2Styled, PStyled, AStyled };
